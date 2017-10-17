f21247a09c0ec0d2855a6070875c36edc9339e1a
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-10-17 12:02:17 -0700

    load app settings

--- SGContent/SGContent/Program.cs
+++ SGContent/SGContent/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SmarterBalanced.SampleItems.Dal.Configurations.Models;
+using System;
+using System.IO;
 
 namespace SGContent
 {
@@ -6,7 +9,16 @@ namespace SGContent
     {
         static void Main(string[] args)
         {
+            var settings = LoadAppSettings();
             Console.WriteLine("Hello World!");
+        }
+
+        static AppSettings LoadAppSettings()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            string appSettingsJson = File.ReadAllText(path);
+            AppSettings settings = JsonConvert.DeserializeObject<AppSettings>(appSettingsJson);
+            return settings;
         }
     }
 }
