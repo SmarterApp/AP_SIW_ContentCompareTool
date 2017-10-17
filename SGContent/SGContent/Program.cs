7dd9f87820f9eb84789c1f4107e630dce6f5350d
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-10-17 11:13:06 -0700

    Create vs project/solution

---
+++ SGContent/SGContent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SGContent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
