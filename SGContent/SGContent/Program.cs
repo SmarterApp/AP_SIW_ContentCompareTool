ad2de3ecc6ca0de353889f2bac417eb46b6ab33c
Alex Lepinski <alex.lepinski@oregonstate.edu>
2017-10-19 14:37:51 -0700

    added logging

--- SGContent/SGContent/Program.cs
+++ SGContent/SGContent/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using SmarterBalanced.SampleItems.Dal.Configurations.Models;
 using System;
@@ -12,12 +13,18 @@ namespace SGContent
     {
         static void Main(string[] args)
         {
+            ILoggerFactory loggerFactory = new LoggerFactory()
+              .AddConsole()
+              .AddDebug();
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+
             string appSettingsUrl = "https://raw.githubusercontent.com/SmarterApp/AP_ItemSampler/master/SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/appsettings.json";
             string itemsPatchUrl = "https://raw.githubusercontent.com/SmarterApp/AP_ItemSampler/master/SmarterBalanced.SampleItems/ClaimConfigurations/ItemsPatch.xml";
             SaveDependency(appSettingsUrl, "appsettings.json");
             SaveDependency(itemsPatchUrl, "ItemsPatch.xml");
 
             Console.WriteLine(Startup.Instance.AppSettings.SbContent.ContentRootDirectory);
+            logger.LogInformation("Test");
         }
 
         static void SaveDependency(string url, string fileName)
